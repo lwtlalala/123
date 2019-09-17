15a154c61cfb84848b4a7b98375cb9f047875778
lwtlalala <55074139+lwtlalala@users.noreply.github.com>
2019-09-17 17:34:57 +0800

    Add files via upload

---
+++ SingletonPatternDemo.java
@@ -0,0 +1,16 @@
+class SingleObject {
+   private static SingleObject instance = new SingleObject();
+   private SingleObject(){}
+   public static SingleObject getInstance(){
+      return instance;
+   }
+   public void showMessage(){
+      System.out.println("Hello World!");
+   }
+}
+public class SingletonPatternDemo{
+   public static void main(String[] args) {
+      SingleObject object = SingleObject.getInstance();
+      object.showMessage();
+   }
+}
